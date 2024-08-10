ffd75a070198b2fb7dddd21dc0af2ea8b9681491
anasfalah3 <anasfalah3@gmail.com>
2024-08-10 13:09:08 +0100

    Blog Page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,11 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime',
+        ];
+    }
 
+    public function author(){
+        return $this->belongsTo(User::class,'user_id');
+    }
     public function scopePublished($query){
         $query->where('published_at','<=',Carbon::now());
     }
     public function scopeFeatured($query){
         $query->where('featured',true);
+    }
+    public function getExcerpt() {
+        return substr(strip_tags($this->body), 0, 200) . '...';
+    }
+    public function getReadingTime() {
+        $words = str_word_count(strip_tags($this->body));
+        $minutes = ceil($words / 200);
+        return $minutes . ' min read';
     }
 }
