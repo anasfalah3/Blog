045ebc19aa25eb9dac3f4a6361def6c798fd8251
anasfalah3 <anasfalah3@gmail.com>
2024-08-11 18:58:09 +0100

    Admin Panel with Filament

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = ['user_id','title','slug','body','image','user_id','published_at','featured'];
     protected function casts(): array
     {
         return [
@@ -18,6 +21,9 @@ class Post extends Model
 
     public function author(){
         return $this->belongsTo(User::class,'user_id');
+    }
+    public function categories(){
+        return $this->belongsToMany(Category::class);
     }
     public function scopePublished($query){
         $query->where('published_at','<=',Carbon::now());
