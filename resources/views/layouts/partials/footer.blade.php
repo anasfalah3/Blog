5a4ca88e17d24f97b48e11977d3fc0af3462df9c
anasfalah3 <anasfalah3@gmail.com>
2024-08-27 23:18:28 +0100

    responsive footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,23 @@
-    <footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 ">
-        <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.about_us') }}</a>
-        <a class="text-gray-500 hover:text-yellow-500" href="">{{ __('menu.profile') }}</a>
-        <a class="text-gray-500 hover:text-yellow-500" href="{{route('posts.index')}}">{{ __('menu.explore') }}</a>
+    <footer class="bg-white border-t-2 border-gray-100 rounded-lg shadow m-4">
+        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <!-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> -->
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap">SyntaxSpark</span>
+                </a>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
+                    <li>
+                        <a href="" class="hover:underline me-4 md:me-6">{{ __('menu.about_us') }}</a>
+                    </li>
+                    <li>
+                        <a href="" class="hover:underline me-4 md:me-6">{{ __('menu.profile') }}</a>
+                    </li>
+                    <li>
+                        <a href="{{route('posts.index')}}" class="hover:underline me-4 md:me-6">{{ __('menu.explore') }}</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center">© 2024 <a href="{{ route('home') }}" class=" hover:underline">SyntaxSpark</a>. All Rights Reserved.</span>
+        </div>
     </footer>
